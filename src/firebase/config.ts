4661a8fa9d8e19d64adc88fc35a62571feb244f5
Andres Montalvo <andresmontalvo2222@gmail.com>
2025-09-25 14:49:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8383780122-c1970",
+  "appId": "1:719676841827:web:5b5956344d81cbedeb3d56",
+  "apiKey": "AIzaSyD6YWK7G9QDOTNIQqCc8A1e2EehfxaAsGs",
+  "authDomain": "studio-8383780122-c1970.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "719676841827"
+};
